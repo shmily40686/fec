4dfb626194648a7dff7f9b19ba234b8fa0db7bab
Frog <shmily40686@gmail.com>
2018-05-20 23:13:29 -0700

    Installed React and ReactDOM for the client Created client folder and basic Hello World Modified server to serve static files Create webpack config file

--- server/index.js
+++ server/index.js
@@ -1,7 +1,10 @@
 const express = require('express')
 const app = express()
+const path = require('path')
 
 const port = process.env.PORT || 3002
+
+app.use(express.static(path.join(__dirname, '../client/dist/')))
 
 app.listen(port, () => {
 	console.log(`listening on port ${port}`)
